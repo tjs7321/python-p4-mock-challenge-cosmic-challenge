1fd5c3a56955cfed36d78b86013447c0a550af0e
Ben Botsford <ben.botsford@flatironschool.com>
2023-04-11 08:13:33 -0500

    finish seed.py

---
+++ server/migrations/versions/141a97b766ec_create_d.py
@@ -0,0 +1,24 @@
+"""create d
+
+Revision ID: 141a97b766ec
+Revises: 
+Create Date: 2023-04-10 18:17:47.697392
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '141a97b766ec'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -20,15 +20,64 @@ class Planet(db.Model, SerializerMixin):
     __tablename__ = 'planets'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    image = db.Column(db.String)
+
+    missions = db.relationship('Mission', backref='planet')
+    scientists = association_proxy('missions', 'scientist',
+        creator=lambda sci: Mission(scientist=sci))
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    field_of_study = db.Column(db.String)
+    avatar = db.Column(db.String)
+
+    missions = db.relationship('Mission', backref='scientist')
+    planets = association_proxy('missions', 'planet',
+        creator=lambda plt: Mission(planet=plt))
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if name:
+            return name
+        raise ValueError('Scientist must have name.')
+    
+    @validates('field_of_study')
+    def validate_name(self, key, field_of_study):
+        if field_of_study:
+            return field_of_study
+        raise ValueError('Scientist must have field of study.')
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if name:
+            return name
+        raise ValueError('Mission must have name.')
+    
+    @validates('scientist_id')
+    def validate_name(self, key, scientist_id):
+        if scientist_id:
+            return scientist_id
+        raise ValueError('Mission must have scientist ID.')
+    
+    @validates('planet_id')
+    def validate_name(self, key, planet_id):
+        if planet_id:
+            return planet_id
+        raise ValueError('Mission must have planet ID.')
 
 # add any models you may need. 
